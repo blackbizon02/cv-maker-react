306581cb021d38fcdf8bb7cc8e45fb00e1bb5ded
blackbizon02 <85622461+blackbizon02@users.noreply.github.com>
2023-02-03 21:14:55 +0400

    first commit

---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./WelcomePage.css"
+
+function WelcomePage() {
+    return (
+        <div className="welcome-page">
+            <div className="container">
+                <img src="/images/logo.png" alt="" />
+                <div className="line"></div>
+                <Link to={'/personal-info'}>
+                    <div className="button">რეზიუმეს დამატება</div>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default WelcomePage
